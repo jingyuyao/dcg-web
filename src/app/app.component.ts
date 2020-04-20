02380fa2561f9ab6a308be2e1f93bf998cbe34f5
Jingyu Yao <yjy.emails@gmail.com>
2020-04-19 21:25:03 -0700

    set up simple websocket connection to show json view of world

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dcg-web';
+  world = {};
+  private subject: WebSocketSubject<any> = webSocket('ws://localhost:8887');
+
+  ngOnInit() {
+    this.subject.subscribe(
+      (msg) => this.handleUpdate(msg),
+      (err) => console.error(err),
+      () => console.log('closed'));
+    this.sendMessage('world');
+  }
+
+  private handleUpdate(world: any) {
+    this.world = world;
+  }
+
+  private sendMessage(command: string, args: number[] = []) {
+    this.subject.next({command, arguments: args});
+  }
 }
