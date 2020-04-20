e2ea65ce5cabd22cd10ecad2c243c9a8f9bb18fc
Jingyu Yao <yjy.emails@gmail.com>
2020-04-19 21:55:47 -0700

    basic world display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,11 @@ import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
 })
 export class AppComponent implements OnInit {
   title = 'dcg-web';
-  world = {};
+  world: any = {};
+  players: any[] = [];
+  forgeRow: any[] = [];
+  units: any[] = [];
+  playedSpells: any[] = [];
   private subject: WebSocketSubject<any> = webSocket('ws://localhost:8887');
 
   ngOnInit() {
@@ -21,6 +25,25 @@ export class AppComponent implements OnInit {
 
   private handleUpdate(world: any) {
     this.world = world;
+    this.players = [];
+    this.forgeRow = [];
+    this.units = [];
+    this.playedSpells = [];
+    for (const [id, entity] of Object.entries<any>(world.entities)) {
+      const archetype = entity.archetype;
+      const components = world.archetypes[archetype];
+      if (components.includes('Player')) {
+        this.players.push(entity);
+      } else if (components.includes('Card') && components.includes('ForgeRow')) {
+        this.forgeRow.push(entity);
+      } else if (components.includes('Unit')) {
+        this.units.push(entity);
+      } else if (components.includes('Spell')) {
+        this.playedSpells.push(entity);
+      } else {
+        console.log(`Unknown entity ${entity}`);
+      }
+    }
   }
 
   private sendMessage(command: string, args: number[] = []) {
