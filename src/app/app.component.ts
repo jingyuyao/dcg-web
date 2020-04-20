ca98baf5bbadf6b60cd2c9970dba3cb35c47007d
Jingyu Yao <yjy.emails@gmail.com>
2020-04-20 16:11:44 -0700

    added card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
+import { Card } from './card/card';
 
 @Component({
   selector: 'app-root',
@@ -10,36 +11,49 @@ export class AppComponent implements OnInit {
   title = 'dcg-web';
   world: any = {};
   players: any[] = [];
-  forgeRow: any[] = [];
+  forgeRow: Card[] = [];
   units: any[] = [];
   playedSpells: any[] = [];
-  private subject: WebSocketSubject<any> = webSocket('ws://localhost:8887');
+  actionMap: Map<string, any> = new Map();
+  private socket: WebSocketSubject<any> = webSocket('ws://localhost:8887');
 
   ngOnInit() {
-    this.subject.subscribe(
+    this.socket.subscribe(
       (msg) => this.handleUpdate(msg),
       (err) => console.error(err),
       () => console.log('closed'));
+    console.log(this.socket);
     this.sendMessage('world');
   }
 
+  selectEntity(id: string) {
+    console.log(id);
+  }
+
   private handleUpdate(world: any) {
+    console.dir(world);
     this.world = world;
     this.players = [];
     this.forgeRow = [];
     this.units = [];
     this.playedSpells = [];
+    this.actionMap = new Map();
     for (const [id, entity] of Object.entries<any>(world.entities)) {
       const archetype = entity.archetype;
       const components = world.archetypes[archetype];
       if (components.includes('Player')) {
         this.players.push(entity);
       } else if (components.includes('Card') && components.includes('ForgeRow')) {
-        this.forgeRow.push(entity);
+        this.forgeRow.push(convertToCard(id, entity));
       } else if (components.includes('Unit')) {
         this.units.push(entity);
       } else if (components.includes('Spell')) {
         this.playedSpells.push(entity);
+      } else if (components.includes('Action')) {
+        const ownerId = entity.components.Owned.owner;
+        const actions = this.actionMap.has(ownerId) ? this.actionMap.get(ownerId) : [];
+        actions.push(entity);
+        this.actionMap.set(ownerId, actions);
       } else {
         console.log(`Unknown entity ${entity}`);
       }
@@ -47,6 +61,14 @@ export class AppComponent implements OnInit {
   }
 
   private sendMessage(command: string, args: number[] = []) {
-    this.subject.next({command, arguments: args});
+    this.socket.next({command, arguments: args});
   }
+}
+
+function convertToCard(id: string, entity: any): Card {
+  return {
+    id,
+    name: entity.components.Common.name,
+    cost: entity.components.Card.cost || 0,
+  };
 }
