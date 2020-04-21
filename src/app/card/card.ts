928a64fe03efb439fe1a996eaf8d524f587f421a
Jingyu Yao <yjy.emails@gmail.com>
2020-04-20 18:58:58 -0700

    show desc

--- src/app/card/card.ts
+++ src/app/card/card.ts
@@ -3,6 +3,7 @@ import { Action } from '../action/action';
 export interface Card {
   id: number;
   name: string;
+  description: string;
   cost: number;
   actions: Action[];
 }
@@ -11,6 +12,7 @@ export function fromCardEntity(id: number, entity: any): Card {
   return {
     id,
     name: entity.components.Common.name,
+    description: entity.components.Common.description,
     cost: entity.components.Card?.cost || 0,
     actions: [],
   };
--- src/app/unit/unit.ts
+++ src/app/unit/unit.ts
@@ -3,6 +3,7 @@ import { Action } from '../action/action';
 export interface Unit {
   id: number;
   name: string;
+  description: string;
   strength: number;
   defense: number;
   attributes: string[];
@@ -14,6 +15,7 @@ export function fromUnitEntity(id: number, entity: any): Unit {
   return {
     id,
     name: entity.components.Common.name,
+    description: entity.components.Common.description,
     strength: unit.strength,
     defense: unit.defense || 0,
     attributes: Object.keys(unit).filter(key => key !== 'strength' && key !== 'defense'),
