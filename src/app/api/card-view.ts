855bd81adfcd5c89682779f9219451f59d69422b
Jingyu Yao <yjy.emails@gmail.com>
2020-04-26 15:52:44 -0700

    nit

--- src/app/api/card-view.ts
+++ src/app/api/card-view.ts
@@ -3,7 +3,7 @@ import { ActionView } from './action-view';
 
 export interface CardView extends EntityView {
   cost: number;
-  canFlash: boolean;
+  canWrap: boolean;
   kind: string;
   colors: string[];
   strength: number;
