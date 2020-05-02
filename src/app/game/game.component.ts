cd03aefd62ad3142a8ddf2b023b96f5e558ad4df
Jingyu Yao <yjy.emails@gmail.com>
2020-05-01 17:07:38 -0700

    use host-context to conditionally hide buttons
    host-context op

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,13 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, HostBinding } from '@angular/core';
 import { GameView } from '../api/game-view';
-import { Observable } from 'rxjs';
+import { Observable, combineLatest } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AttachmentView } from '../api/attachment-view';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.sass'],
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
+  @Input() attachmentView$: Observable<AttachmentView>;
   // TODO: need to show played units as voidbindable
   @Input() gameview$: Observable<GameView>;
+  @HostBinding('class.canAct') canAct: boolean;
+
+  ngOnInit(): void {
+    combineLatest([this.attachmentView$, this.gameview$])
+      .pipe(
+        map(([attachment, game]) => {
+          const currentPlayer = game.players.find((player) => player.isCurrent);
+          return attachment.playerName === currentPlayer?.name;
+        })
+      )
+      .subscribe((canAct) => (this.canAct = canAct));
+  }
 }
