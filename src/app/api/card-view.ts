b0965e54b8de144244a15239e5e7e9cdb516719e
Jingyu Yao <yjy.emails@gmail.com>
2020-07-07 13:25:38 -0700

    update api to include card location

--- src/app/api/card-view.ts
+++ src/app/api/card-view.ts
@@ -5,6 +5,7 @@ export interface CardView extends EntityView {
   cost: number;
   canWrap: boolean;
   kind: CardKind;
+  location: CardLocation;
   colors: string[];
   strength: number;
   actions: ActionView[];
@@ -15,4 +16,16 @@ export enum CardKind {
   BASIC = 'BASIC',
   SPELL = 'SPELL',
   UNIT = 'UNIT',
+}
+
+export enum CardLocation {
+  UNKNOWN = 'UNKNOWN',
+  FORGE_DECK = 'FORGE_DECK',
+  FORGE_ROW = 'FORGE_ROW',
+  THRONE_DECK = 'THRONE_DECK',
+  MERCENARY_DECK = 'MERCENARY_DECK',
+  PLAYER_DECK = 'PLAYER_DECK',
+  DISCARD_PILE = 'DISCARD_PILE',
+  HAND = 'HAND',
+  PLAY_AREA = 'PLAY_AREA',
 }
