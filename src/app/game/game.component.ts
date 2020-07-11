46b9ebb8cafaee4bbb7e838d1b615bf0e71e32a5
Jingyu Yao <yjy.emails@gmail.com>
2020-07-11 13:28:33 -0700

    new forge layout & switch to using the new api

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit, HostBinding } from '@angular/core';
 import { GameView } from '../api/game-view';
 import { Observable, combineLatest } from 'rxjs';
 import { AttachmentView } from '../api/attachment-view';
+import { CardView, CardKind, CardLocation } from '../api/card-view';
 
 @Component({
   selector: 'app-game',
@@ -10,11 +11,13 @@ import { AttachmentView } from '../api/attachment-view';
 })
 export class GameComponent implements OnInit {
   @Input() attachmentView$: Observable<AttachmentView>;
-  // TODO: need to show played units as voidbindable
   @Input() gameview$: Observable<GameView>;
   @HostBinding('class.canAct') canAct: boolean;
   previousPlayerName: string;
   currentPlayerName: string;
+  forge: CardView[];
+  numThrone: number;
+  numMercenary: number;
 
   ngOnInit(): void {
     combineLatest([this.attachmentView$, this.gameview$]).subscribe(
@@ -30,5 +33,40 @@ export class GameComponent implements OnInit {
             : game.currentPlayerName + `'s`;
       }
     );
+    this.gameview$.subscribe((game) => {
+      this.updateForge(game);
+    });
+  }
+
+  private updateForge(game: GameView) {
+    this.forge = [];
+    this.numThrone = 0;
+    this.numMercenary = 0;
+    for (const card of game.cards) {
+      if (card.location === CardLocation.FORGE_ROW) {
+        this.forge.push(card);
+      }
+      if (card.location === CardLocation.THRONE_DECK) {
+        if (this.numThrone === 0) {
+          this.forge.push(card);
+        }
+        this.numThrone++;
+      }
+      if (card.location === CardLocation.MERCENARY_DECK) {
+        if (this.numMercenary === 0) {
+          this.forge.push(card);
+        }
+        this.numMercenary++;
+      }
+    }
+    this.forge.sort((c1, c2) => (
+      FORGE_ORDER.indexOf(c1.location) - FORGE_ORDER.indexOf(c2.location)
+    ));
   }
 }
+
+const FORGE_ORDER = [
+  CardLocation.THRONE_DECK,
+  CardLocation.MERCENARY_DECK,
+  CardLocation.FORGE_ROW,
+];
