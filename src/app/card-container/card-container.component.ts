0ff01c838230bf58cd6af055151b835dcf779c34
Jingyu Yao <yjy.emails@gmail.com>
2020-07-13 18:37:38 -0700

    added the ability to dedupe to card container
    used it in forge and discard pile yay

--- src/app/card-container/card-container.component.ts
+++ src/app/card-container/card-container.component.ts
@@ -1,36 +1,62 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
 import { CardView } from '../api/card-view';
 
 export interface State {
   card: CardView;
   fadeIn: boolean;
+  count?: number;
 }
 
 @Component({
   selector: 'app-card-container',
   templateUrl: './card-container.component.html',
-  styleUrls: ['./card-container.component.sass']
+  styleUrls: ['./card-container.component.sass'],
 })
 export class CardContainerComponent implements OnInit, OnChanges {
   @Input() cards: CardView[];
   @Input() canAct: boolean;
-  cardStates: State[];
+  @Input() dedupe = false;
+  cardStates: State[] = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-    this.cardStates = this.cards.map((card) => ({
-      card,
-      fadeIn: true,
-    }));
+    this.cardStates = this.computeStates(this.cards, this.cardStates);
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    const previousCards: CardView[] = changes.cards.previousValue;
     const currentCards: CardView[] = changes.cards.currentValue;
-    this.cardStates = currentCards.map((card) => ({
-      card,
-      fadeIn: !previousCards.find((c) => c.id === card.id),
-    }));
+    this.cardStates = this.computeStates(currentCards, this.cardStates);
+  }
+
+  private computeStates(cards: CardView[], currentStates: State[]): State[] {
+    const states: State[] = [];
+    for (const card of cards) {
+      const fadeIn = !currentStates.find((s) => s.card.id === card.id);
+      if (this.dedupe) {
+        const existingState = states.find((s) => s.card.name === card.name);
+        if (existingState) {
+          existingState.count++;
+        } else {
+          states.push({
+            card,
+            fadeIn,
+            count: 1,
+          });
+        }
+      } else {
+        states.push({
+          card,
+          fadeIn,
+        });
+      }
+    }
+    return states;
   }
 }
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -5,17 +5,6 @@ import { CardView, CardLocation, CardKind } from '../api/card-view';
 import { UnitView, UnitState } from '../api/unit-view';
 import { PlayerView } from '../api/player-view';
 
-interface CardViewUI extends CardView {
-  fadeIn: boolean;
-}
-
-// NOTE: somehow angular doesn't update when we change individual references to
-// a property.
-interface ForgeGroup {
-  card: CardViewUI;
-  count: number;
-}
-
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
@@ -29,7 +18,7 @@ export class GameComponent implements OnInit {
   deckSize = 0;
   throneActive = false;
   players: PlayerView[] = [];
-  forgeGroups: ForgeGroup[] = [];
+  forgeDedupe: CardView[] = [];
   forge: CardView[] = [];
   playArea: CardView[] = [];
   hand: CardView[] = [];
@@ -64,56 +53,21 @@ export class GameComponent implements OnInit {
   }
 
   private updateForge(game: GameView) {
-    const previousForgeGroups = this.forgeGroups;
+    this.forgeDedupe = [];
     this.forge = [];
-    this.forgeGroups = [];
-    let throne = null;
-    let numThrone = 0;
-    let mercenary = null;
-    let numMercenary = 0;
     for (const card of game.cards) {
       switch (card.location) {
         case CardLocation.FORGE_ROW:
           this.forge.push(card);
           break;
         case CardLocation.THRONE_DECK:
-          const previousThrone = previousForgeGroups.find(
-            (g) => g.card.id === card.id
-          );
-          if (throne == null) {
-            throne = {
-              ...card,
-              fadeIn: !previousThrone,
-            };
-          }
-          numThrone++;
-          break;
         case CardLocation.MERCENARY_DECK:
-          const previousMercenary = previousForgeGroups.find(
-            (g) => g.card.id === card.id
-          );
-          if (mercenary == null) {
-            mercenary = {
-              ...card,
-              fadeIn: !previousMercenary,
-            };
-          }
-          numMercenary++;
+          this.forgeDedupe.push(card);
           break;
       }
     }
-    if (throne) {
-      this.forgeGroups.push({ card: throne, count: numThrone });
-      this.throneActive = false;
-    } else {
-      this.throneActive = true;
-    }
-    if (mercenary) {
-      this.forgeGroups.push({ card: mercenary, count: numMercenary });
-    }
-    this.forge.sort(
-      (c1, c2) =>
-        FORGE_ORDER.indexOf(c1.location) - FORGE_ORDER.indexOf(c2.location)
+    this.throneActive = !this.forgeDedupe.find(
+      (c) => c.location === CardLocation.THRONE_DECK
     );
   }
 
@@ -153,9 +107,3 @@ export class GameComponent implements OnInit {
     }
   }
 }
-
-const FORGE_ORDER = [
-  CardLocation.THRONE_DECK,
-  CardLocation.MERCENARY_DECK,
-  CardLocation.FORGE_ROW,
-];
