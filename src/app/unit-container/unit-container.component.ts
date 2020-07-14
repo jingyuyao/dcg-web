ef8cc25f2f9d4efacaa0a054b3084d6d08758de4
Jingyu Yao <yjy.emails@gmail.com>
2020-07-13 19:05:15 -0700

    added unit stat highlight

--- src/app/unit-container/unit-container.component.ts
+++ src/app/unit-container/unit-container.component.ts
@@ -1,22 +1,29 @@
-import { Component, OnInit, OnChanges, SimpleChanges, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnChanges,
+  SimpleChanges,
+  Input,
+} from '@angular/core';
 import { UnitView } from '../api/unit-view';
 
 export interface State {
   unit: UnitView;
+  previousUnit?: UnitView;
   fadeIn: boolean;
 }
 
 @Component({
   selector: 'app-unit-container',
   templateUrl: './unit-container.component.html',
-  styleUrls: ['./unit-container.component.sass']
+  styleUrls: ['./unit-container.component.sass'],
 })
 export class UnitContainerComponent implements OnInit, OnChanges {
   @Input() units: UnitView[];
   @Input() canAct: boolean;
   unitStates: State[];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     this.unitStates = this.units.map((unit) => ({
@@ -28,9 +35,13 @@ export class UnitContainerComponent implements OnInit, OnChanges {
   ngOnChanges(changes: SimpleChanges): void {
     const previousUnits: UnitView[] = changes.units.previousValue;
     const currentUnits: UnitView[] = changes.units.currentValue;
-    this.unitStates = currentUnits.map((unit) => ({
-      unit,
-      fadeIn: !previousUnits.find((u) => u.id === unit.id),
-    }));
+    this.unitStates = currentUnits.map((unit) => {
+      const previousUnit = previousUnits.find((u) => u.id === unit.id);
+      return {
+        unit,
+        previousUnit,
+        fadeIn: !previousUnit,
+      };
+    });
   }
 }
--- src/app/unit/unit.component.ts
+++ src/app/unit/unit.component.ts
@@ -1,17 +1,45 @@
-import { Component, Input, HostListener, HostBinding } from '@angular/core';
+import {
+  Component,
+  Input,
+  HostListener,
+  HostBinding,
+  OnInit,
+} from '@angular/core';
 import { UnitView } from '../api/unit-view';
 
 @Component({
   selector: 'app-unit',
   templateUrl: './unit.component.html',
-  styleUrls: ['./unit.component.sass']
+  styleUrls: ['./unit.component.sass'],
 })
-export class UnitComponent {
+export class UnitComponent implements OnInit {
   @Input() unit: UnitView;
+  @Input() previousUnit?: UnitView;
   @Input() canAct: boolean;
   @Input() @HostBinding('class.fade-in') fadeIn: boolean;
+  strengthChanged: boolean;
+  defenseChanged: boolean;
+  colorChanged: boolean;
+  attributesChanged: boolean;
 
   @HostListener('click') onClick() {
     console.dir(this.unit);
+  }
+
+  ngOnInit(): void {
+    if (!this.previousUnit) {
+      return;
+    }
+
+    this.strengthChanged = this.previousUnit.strength !== this.unit.strength;
+    this.defenseChanged = this.previousUnit.defense !== this.unit.defense;
+    this.colorChanged =
+      this.previousUnit.colors.length !== this.unit.colors.length ||
+      this.previousUnit.colors.some((c) => !this.unit.colors.includes(c));
+    this.attributesChanged =
+      this.previousUnit.attributes.length !== this.unit.attributes.length ||
+      this.previousUnit.attributes.some(
+        (a) => !this.unit.attributes.includes(a)
+      );
   }
 }
